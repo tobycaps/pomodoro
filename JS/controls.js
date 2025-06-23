933b728c38ea3af5dadb4d142a46ccd4f79f4f19
tobycaps <tcap745@gmail.com>
2025-06-23 20:12:23 +0800

    stop btn added

--- JS/controls.js
+++ JS/controls.js
@@ -1,7 +1,7 @@
 const startControls = document.getElementById('start-controls');
 const startButton = document.getElementById('start-btn');
 const fullControls = document.getElementById('full-controls');
-const resetButton = document.getElementById('reset-btn');
+const stopButton = document.getElementById('stop-btn');
 const pauseButton = document.getElementById('pause-btn');
 const resumeButton = document.getElementById('resume-btn');
 const breakButton = document.getElementById('break-btn');
@@ -10,10 +10,13 @@ const breakButton = document.getElementById('break-btn');
 startButton.addEventListener('click', startTimer);
 pauseButton.addEventListener('click', pauseTimer);
 resumeButton.addEventListener('click', resumeTimer);
+stopButton.addEventListener('click', stopTimer);
 
 function startTimer() {
     startControls.classList.remove('active');
     fullControls.classList.add('active');
+    pauseButton.classList.add('active');
+    resumeButton.classList.remove('active');
 }
 
 function pauseTimer() {
@@ -24,4 +27,9 @@ function pauseTimer() {
 function resumeTimer() {
     pauseButton.classList.add('active');
     resumeButton.classList.remove('active');
+}
+
+function stopTimer() {
+    fullControls.classList.remove('active');
+    startControls.classList.add('active');
 }
